c0fcc1803c91599a9b65ef1185d780f6686b2329
Fairuz Ayuni <63067371+ayuni04@users.noreply.github.com>
2025-10-28 11:28:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 # Set the title and icon for the page
 st.set_page_config(
-    page_title="Program Apprentice 3u1i@FHPK",
-    page_icon="🎓", 
+    page_title="Program Apprentice 3u1i@FHPK", #untuk header taskbar
+    page_icon="🎓", #untuk header taskbar
 )
 
-analysis = st.Page('PLO_sum.py', title='Pencapaian Akademik', icon=":material/school:")
+#analysis = st.Page('PLO_sum.py', title='Pencapaian Akademik', icon=":material/school:")
 #overall = st.Page('Overall_PLO.py', title="Overall PLO")
-industri = st.Page('Industry_List.py', title='Pencapaian Industri', icon=":material/business:")
+#industri = st.Page('Industry_List.py', title='Pencapaian Industri', icon=":material/business:")
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
