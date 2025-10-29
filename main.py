311987506d0b3500a250b2764d333c239a1b67d6
Fairuz Ayuni <63067371+ayuni04@users.noreply.github.com>
2025-10-29 11:14:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ st.set_page_config(
 
 #analysis = st.Page('PLO_sum.py', title='Pencapaian Akademik', icon=":material/school:")
 #overall = st.Page('Overall_PLO.py', title="Overall PLO")
+genetic = st.Page('ga.py', title='Genetic Algorithm', icon=":material/business:")
 travelling = st.Page('tsp.py', title='Travelling Salesman', icon=":material/business:")
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
         {
-            "Menu": [home,travelling]
+            "Menu": [home,travelling,genetic]
         }
     )
 
