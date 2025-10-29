338290cea2e94b5c4c21fc132bdb943d37a0a817
Zamarul <zamarulhisyam1611@gmail.com>
2025-10-29 17:48:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.set_page_config(
 )
 
 #analysis = st.Page('PLO_sum.py', title='Pencapaian Akademik', icon=":material/school:")
-#overall = st.Page('Overall_PLO.py', title="Overall PLO")
+#overall = st.Page('Overall_PLO.py', title="Overall PLO"),
 genetic = st.Page('ga.py', title='Genetic Algorithm', icon=":material/business:")
 travelling = st.Page('tsp.py', title='Travelling Salesman', icon=":material/business:")
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
