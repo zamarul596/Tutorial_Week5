b2cc62d4260c1802b13f7be21754b71c721d1824
Fairuz Ayuni <63067371+ayuni04@users.noreply.github.com>
2025-10-29 11:37:50 +0800

    Update ga.py

--- ga.py
+++ ga.py
@@ -119,11 +119,11 @@ def main(POP_SIZE, MUT_RATE, TARGET, GENES):
 
 
       if (population[0][1] == 0):
-        print('Target found')
-        print('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
+        st.write('Target found')
+        st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
         break
-      print('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
+      st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
       generation+=1
 
 result = main(POP_SIZE, MUT_RATE, TARGET, GENES)
-st.write(result)
+#st.write(result)
