eb7f810a3b24dd3ad07cb261135250d9456f97c8
Fairuz Ayuni <63067371+ayuni04@users.noreply.github.com>
2025-10-29 11:42:33 +0800

    Update ga.py

--- ga.py
+++ ga.py
@@ -8,7 +8,7 @@ POP_SIZE = 500
 MUT_RATE = 0.2
 
 #TARGET: Our goal.
-TARGET = 'fakhitah'
+TARGET = 'Ayuni'
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyz'
